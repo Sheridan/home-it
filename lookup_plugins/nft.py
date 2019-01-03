d338b052387dda196e493cf4fdcd30904b38d73b
Горлов Максим <gorlov.maxim@gmail.com>
2019-01-04 00:28:45 +0300

    iptables->nftables

---
+++ lookup_plugins/nft.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+# import netaddr
+
+
+try:
+    from __main__ import display
+except ImportError:
+    from ansible.utils.display import Display
+    display = Display()
+
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+        display.v("Netfilter terms: %s" % terms)
+        try:
+            if terms[0] == 'as_iif':  # 1 == interface
+                return self.as_if('i', terms[1])
+            if terms[0] == 'as_oif':  # 1 == interface
+                return self.as_if('o', terms[1])
+            if terms[0] == 'list_or_single':  # 1 == list
+                return self.as_list_or_single(terms[1])
+            raise AnsibleParserError("Непонятно что делать")
+        except Exception as e:
+            raise AnsibleError("Error in nft: %s (%s)" % (terms, e))
+
+    def as_if(self, direction, interface):
+        return '{0}if {1}'.format(direction, interface['ifname']) if interface['type'] == 'static' else '{0}ifname "{1}"'.format(direction, interface['ifname'])
+
+    def as_list_or_single(self, items):
+        if isinstance(items, list):
+            if len(items) > 1:
+                return '{ %s }' % ', '.join(str(x) for x in items)
+            return items[0]
+        return items
