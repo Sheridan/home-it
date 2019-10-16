1b1b95a8e61b35f01cb39f5c40dc7a9e93866fb4
Горлов Максим <gorlov.maxim@gmail.com>
2019-10-16 23:07:25 +0300

    Скрипт удаления из истории

--- tools/remove-file-from-git-history.sh
+++ tools/remove-file-from-git-history.sh
@@ -7,8 +7,8 @@ tmp_filename=$(date +"%Y_%m_%d_%H_%M_%S")
 mkdir -p ${tmp_dir}
 cat ${filename} > ${tmp_dir}/${tmp_filename}
 git rm --cached ${filename}
-git commit --amend -CHEAD -m "Removing ${tmp_filename} from history"
+git commit --amend -CHEAD
 cat ${tmp_dir}/${tmp_filename} > ${filename}
 git add ${filename}
-git commit --amend -CHEAD -m "Restoring ${tmp_filename}"
+git commit --amend -CHEAD -m "Removing ${tmp_filename} from history"
 git push
