a535a5bdd1f12d0db05c4c802f9715be5fda7be3
Горлов Максим <gorlov.maxim@gmail.com>
2019-10-16 23:32:27 +0300

    Скрипт удаления из истории

--- tools/remove-file-from-git-history.sh
+++ tools/remove-file-from-git-history.sh
@@ -11,5 +11,5 @@ git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch ${filenam
 git push --force
 cat ${tmp_dir}/${tmp_filename} > ${filename}
 git add ${filename}
-git commit -m "Removing ${tmp_filename} from history"
+git commit -m "✖ Removing `${filename}` from history"
 git push
