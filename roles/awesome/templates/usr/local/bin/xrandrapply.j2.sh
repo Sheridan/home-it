bf02e5b1ca1a8dc2d7f1dd26b03d8fe71834b266
Горлов Максим <gorlov.maxim@gmail.com>
2020-10-11 12:25:52 +0300

    started boink,wiki; new monitor

--- roles/awesome/templates/usr/local/bin/xrandrapply.j2.sh
+++ roles/awesome/templates/usr/local/bin/xrandrapply.j2.sh
@@ -17,10 +17,10 @@ xrandr \
     {# -#}
     {%- endfor %}
 
-xrandr \
+xrandr --verbose \
     --dpi {{ merged_vars.Xorg.options.dpi }} \
     {% for monitor_name in merged_vars.Xorg.options.init_order -%}
       {%- set monitor = merged_vars.Xorg.monitors[monitor_name] -%}
-    --output "{{ monitor.video_out }}" --mode {{ monitor.mode.width }}x{{ monitor.mode.height }} --rate {{ monitor.mode.rate }} {% if 'base' not in monitor.position %} --{{ monitor.position.where }} "{{ merged_vars.Xorg.monitors[monitor.position.from].video_out }}" {% else %} --primary {% endif -%} \
+    --output "{{ monitor.video_out }}" --mode {{ monitor.mode.width }}x{{ monitor.mode.height }} --rate {{ monitor.mode.rate }} {% if 'base' not in monitor.position %} --{{ monitor.position.where }} "{{ merged_vars.Xorg.monitors[monitor.position.from].video_out }}" {% else %} --primary {% endif %}{% if 'rotate' in monitor %}--rotate {{ monitor.rotate }}{% endif %} \
     {# -#}
     {%- endfor %}
