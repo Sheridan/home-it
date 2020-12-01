92375092852b583ec0cbcce479ba33a83a81cf7a
Горлов Максим <gorlov.maxim@gmail.com>
2020-12-01 20:38:34 +0300

    new role: grub; new devices; upgraded xorg

--- roles/xorg/templates/usr/local/bin/xrandrapply.j2.sh
+++ roles/xorg/templates/usr/local/bin/xrandrapply.j2.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-{% if merged_vars.Xorg.videocards | length > 1 %}
+{% if merged_vars.xorg.videocards | length > 1 %}
 xrandr --setprovideroutputsource 1 0
 {% endif %}
 
-{% for monitor_name in merged_vars.Xorg.options.init_order.monitors -%}
-  {%- set monitor = merged_vars.Xorg.monitors[monitor_name] -%}
+{% for monitor_name in merged_vars.xorg.options.init_order.monitors -%}
+  {%- set monitor = merged_vars.xorg.monitors[monitor_name] -%}
 # {{ monitor_name }}: {{ monitor.vendor }} {{ monitor.model }} -> {{ monitor.mode.width }}x{{ monitor.mode.height }}@{{ monitor.mode.rate }}
 {# -#}
 {%- endfor %}
 
 {#
 xrandr \
-    {% for monitor_name in merged_vars.Xorg.options.init_order.monitors -%}
-      {%- set monitor = merged_vars.Xorg.monitors[monitor_name] -%}
+    {% for monitor_name in merged_vars.xorg.options.init_order.monitors -%}
+      {%- set monitor = merged_vars.xorg.monitors[monitor_name] -%}
     --output "{{ monitor.connector.out }}" --off \
     # -#
     {%- endfor %}
 #}
 
 xrandr --verbose \
-    --dpi {{ merged_vars.Xorg.options.dpi }} \
-    {% for monitor_name in merged_vars.Xorg.options.init_order.monitors -%}
-      {%- set monitor = merged_vars.Xorg.monitors[monitor_name] -%}
-    --output "{{ monitor.connector.out }}" --mode {{ monitor.mode.width }}x{{ monitor.mode.height }} --rate {{ monitor.mode.rate }} {% if 'base' not in monitor.position %} --{{ monitor.position.where }} "{{ merged_vars.Xorg.monitors[monitor.position.from].connector.out }}" {% else %} --primary {% endif %}{% if 'rotate' in monitor %}--rotate {{ monitor.rotate }}{% endif %} \
+    --dpi {{ merged_vars.xorg.options.dpi }} \
+    {% for monitor_name in merged_vars.xorg.options.init_order.monitors -%}
+      {%- set monitor = merged_vars.xorg.monitors[monitor_name] -%}
+    --output "{{ monitor.connector.out }}" --mode {{ monitor.mode.width }}x{{ monitor.mode.height }} --rate {{ monitor.mode.rate }} {% if 'base' not in monitor.position %} --{{ monitor.position.where }} "{{ merged_vars.xorg.monitors[monitor.position.from].connector.out }}" {% else %} --primary {% endif %}{% if 'rotate' in monitor %}--rotate {{ monitor.rotate }}{% endif %} \
     {# -#}
     {%- endfor %}
